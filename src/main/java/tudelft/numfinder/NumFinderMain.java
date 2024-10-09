65f0af25f14e2a291fe2e49725b520e30790960d
Nazareth1um <nazaretoz.ups789@gmail.com>
2024-10-09 20:54:05 +0300

    Исправлена обработка массива и добавлена проверка на null и пустой массив

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -1,17 +1,18 @@
-package tudelft.numfinder;
-
 public class NumFinderMain {
-
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
-
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
-
+        nf.find(new int[] {4, 25, 7, 9});
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
+
+        nf.find(new int[] {4, 3, 2, 1});
+        System.out.println(nf.getLargest());
+        System.out.println(nf.getSmallest());
+
+        nf.find(null);
+
+        nf.find(new int[] {});
     }
 }
+
